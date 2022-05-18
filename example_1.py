0c404d15b57c2c9c446d0c8ec306a91769dd429c
sebastiandres <sebastiandres@gmail.com>
2022-05-18 13:18:00 -0400

    testing

--- example_1.py
+++ example_1.py
@@ -6,7 +6,11 @@ st.title("Page title and favicon example")
 
 html = """
 <script>
+elements = window.parent.document.querySelectorAll('.stMarkdown');
+//alert(elements.length);
+elements[0].style.backgroundColor="red";
 window.parent.document.title = "ABC"
+elements[0].style.backgroundColor="blue";
 </script>
 """
 st.components.v1.html(html)
