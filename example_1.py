0771e5c8def6d62dcddc64aa5d75ef118fc58755
sebastiandres <sebastiandres@gmail.com>
2022-05-18 13:18:46 -0400

    testing 2

--- example_1.py
+++ example_1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title='XYZ', page_icon = 'favicon.ico')
+st.set_page_config(page_icon = 'favicon.ico')
 
 st.title("Page title and favicon example")
 
