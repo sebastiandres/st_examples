443e7883e16b29f9e2835b03f5bba295e72db351
sebastiandres <sebastiandres@gmail.com>
2022-05-18 09:09:44 -0400

    dynamically changing title

--- example_1.py
+++ example_1.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.set_page_config(page_title='XYZ', page_icon = 'favicon.ico')
 
 st.title("Page title and favicon example")
+
+html = """
+<script>
+window.parent.document.title = "ABC"
+</script>
+"""
+st.components.v1.html(html)
